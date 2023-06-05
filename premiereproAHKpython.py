e318e150ef007a7d41be6e0d53790805a32272a4
Joseph Ongchangco <43042517+kiyros@users.noreply.github.com>
2023-06-05 15:35:53 -0400

    updated for AHK V2
    scale and x position updated enough to be used

--- premiereproAHKpython.py
+++ premiereproAHKpython.py
@@ -1,19 +1,28 @@
-from ahk import AHK, Hotkey
+from ahk import AHK
 
 # autoHotkey wrapper for python
 ahk = AHK()
 win = ahk.active_window
 
+#hotkeys modifiers---------
+# # windows key
+# ! alt key
+# ^ ctrl
+# + shift
 
-def hotkeys():
-    scale_hotkey = Hotkey(ahk, '!^s', 'preimereHotkeys.ahk')
-    scale_hotkey.start()
 
 
 def scale():
     ahk.mouse_move(200, 500, speed=15)
 
+def my_ex_handler(hotkey: str, exception: Exception):
+    print('exception with callback for hotkey', hotkey, 'Here was the error:', exception)
 
-while True:
-    if ahk.key_state('s') and ahk.key_state('Control'):
-        print("success")
+def go_boom():
+    raise Exception('boom!')
+
+
+
+ahk.add_hotkey('+n', callback=go_boom, ex_handler=my_ex_handler)
+ahk.start_hotkeys()
+ahk.block_forever()
